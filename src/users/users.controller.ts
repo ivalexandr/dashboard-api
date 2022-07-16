37e8eb8f176b37d10af68d87cbda9fe3316b1bcf
Ivanin Alexandr <ivanin.alexandr1994@yandex.ru>
2022-07-16 22:21:00 +0300

    added user entity

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -8,6 +8,7 @@ import { ILogger } from '../logger/logger.interface'
 import { IUserController } from './users.interface'
 import { UserLoginDto } from './dto/user-login.dto'
 import { UserRegisterDto } from './dto/user-register.dto'
+import { User } from './user.entity'
 import 'reflect-metadata'
 
 @injectable()
@@ -35,8 +36,13 @@ export class UserController extends BaseController implements IUserController {
 		next(new HTTPError(401, 'Ошибка авторизации', 'login'))
 	}
 
-	register = (req: Request<{}, {}, UserRegisterDto>, res: Response, next: NextFunction): void => {
-		console.log(req.body)
-		this.ok(res, 'register')
+	register = async (
+		{ body }: Request<{}, {}, UserRegisterDto>,
+		res: Response,
+		next: NextFunction,
+	): Promise<void> => {
+		const newUser = new User(body.email, body.name)
+		await newUser.setPassword(body.password)
+		this.ok(res, newUser)
 	}
 }
